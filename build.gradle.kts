f1e8c28d61eae5cfe304884098d3ac869d51e6b9
Sylvain Becuwe <sylvain_becuwe@connect-tech.sncf>
2025-03-25 18:49:51 +0100

    bump: Bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
 
 plugins {
-    kotlin("jvm") version "1.9.23"
-    id("org.openapi.generator") version "7.3.0"
+    kotlin("jvm") version "2.1.20"
+    id("org.openapi.generator") version "7.12.0"
 }
 
 group = "com.grassehh"
@@ -21,7 +21,7 @@ tasks.compileKotlin {
 }
 
 task<GenerateTask>("openApiGenerateKotlin") {
-    generatorName.set("kotlin")
+    generatorName.set("kotlin-spring")
     inputSpec.set("openapi.yaml")
     outputDir.set("${layout.buildDirectory.asFile.get()}/generated")
     modelPackage.set("com.grassehh.api.model")
